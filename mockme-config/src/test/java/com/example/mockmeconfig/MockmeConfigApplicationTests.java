415d8bb300a865424eb30baf10869f901459f198
Amal Janabayev <amalj2426@gmail.com>
2022-02-15 12:56:15 -0800

    Updated config server

---
+++ mockme-config/src/test/java/com/example/mockmeconfig/MockmeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockmeconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockmeConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
